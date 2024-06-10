9806b01e9e7605615f07b2461d72c2120f372785
Canal Vem Fazer <156688549+canalvemfazer@users.noreply.github.com>
2024-06-10 10:43:50 -0300

    Update index.js

--- frontend/src/pages/Connections/index.js
+++ frontend/src/pages/Connections/index.js
@@ -1,7 +1,6 @@
-import React, { useState, useCallback, useContext, useEffect } from "react";
+import React, { useState, useCallback, useContext } from "react";
 import { toast } from "react-toastify";
-import { add, format, parseISO } from "date-fns";
-import { useHistory } from "react-router-dom";
+import { format, parseISO } from "date-fns";
 
 import { makeStyles } from "@material-ui/core/styles";
 import { green } from "@material-ui/core/colors";
@@ -17,9 +16,6 @@ import {
 	Tooltip,
 	Typography,
 	CircularProgress,
-	Box,
-	Card,
-	CardContent,
 } from "@material-ui/core";
 import {
 	Edit,
@@ -44,11 +40,9 @@ import QrcodeModal from "../../components/QrcodeModal";
 import { i18n } from "../../translate/i18n";
 import { WhatsAppsContext } from "../../context/WhatsApp/WhatsAppsContext";
 import toastError from "../../errors/toastError";
-import { socketConnection } from "../../services/socket";
 
 import { AuthContext } from "../../context/Auth/AuthContext";
 import { Can } from "../../components/Can";
-import { WhatsApp } from "@material-ui/icons";
 
 const useStyles = makeStyles(theme => ({
 	mainPaper: {
@@ -103,11 +97,9 @@ const CustomToolTip = ({ title, content, children }) => {
 
 const Connections = () => {
 	const classes = useStyles();
-	const history = useHistory();
 
 	const { user } = useContext(AuthContext);
 	const { whatsApps, loading } = useContext(WhatsAppsContext);
-	const [statusImport, setStatusImport] = useState([]);
 	const [whatsAppModalOpen, setWhatsAppModalOpen] = useState(false);
 	const [qrModalOpen, setQrModalOpen] = useState(false);
 	const [selectedWhatsApp, setSelectedWhatsApp] = useState(null);
@@ -122,25 +114,6 @@ const Connections = () => {
 	const [confirmModalInfo, setConfirmModalInfo] = useState(
 		confirmationModalInitialState
 	);
-
-	useEffect(() => {
-		const companyId = localStorage.getItem("companyId");
-		const socket = socketConnection({ companyId });
-
-		socket.on(`importMessages-${user.companyId}`, (data) => {
-			if (data.action === "refresh") {
-				setStatusImport([]);
-				history.go(0);
-			}
-			if (data.action === "update") {
-				setStatusImport(data.status);
-			}
-		});
-
-		/* return () => {
-		  socket.disconnect();
-		}; */
-	}, [whatsApps]);
 
 	const handleStartWhatsAppSession = async whatsAppId => {
 		try {
@@ -158,11 +131,6 @@ const Connections = () => {
 		}
 	};
 
-	const openInNewTab = url => {
-		window.open(url, '_blank', 'noopener,noreferrer');
-	};
-
-	  
 	const handleOpenWhatsAppModal = () => {
 		setSelectedWhatsApp(null);
 		setWhatsAppModalOpen(true);
@@ -206,14 +174,6 @@ const Connections = () => {
 				whatsAppId: whatsAppId,
 			});
 		}
-		if (action === "closedImported") {
-			setConfirmModalInfo({
-				action: action,
-				title: i18n.t("connections.confirmationModal.closedImportedTitle"),
-				message: i18n.t("connections.confirmationModal.closedImportedMessage"),
-				whatsAppId: whatsAppId,
-			});
-		}
 		setConfirmModalOpen(true);
 	};
 
@@ -235,92 +195,7 @@ const Connections = () => {
 			}
 		}
 
-		if (confirmModalInfo.action === "closedImported") {
-			try {
-				await api.post(`/closedimported/${confirmModalInfo.whatsAppId}`);
-				toast.success(i18n.t("connections.toasts.closedimported"));
-			} catch (err) {
-				toastError(err);
-			}
-		}
-
 		setConfirmModalInfo(confirmationModalInitialState);
-	};
-
-	function CircularProgressWithLabel(props) {
-		return (
-			<Box position="relative" display="inline-flex">
-				<CircularProgress variant="determinate" {...props} />
-				<Box
-					top={0}
-					left={0}
-					bottom={0}
-					right={0}
-					position="absolute"
-					display="flex"
-					alignItems="center"
-					justifyContent="center"
-				>
-					<Typography
-						variant="caption"
-						component="div"
-						color="textSecondary"
-					>{`${Math.round(props.value)}%`}</Typography>
-				</Box>
-			</Box>
-		);
-	}
-
-	const renderImportButton = (whatsApp) => {
-		if (whatsApp?.statusImportMessages === "renderButtonCloseTickets") {
-			return (
-				<Button
-					style={{ marginLeft: 12 }}
-					size="small"
-					variant="outlined"
-					color="primary"
-					onClick={() => {
-						handleOpenConfirmationModal("closedImported", whatsApp.id);
-					}}
-				>
-					{i18n.t("connections.buttons.closedImported")}
-				</Button>
-			);
-		}
-
-		if (whatsApp?.importOldMessages) {
-			let isTimeStamp = !isNaN(
-				new Date(Math.floor(whatsApp?.statusImportMessages)).getTime()
-			);
-
-			if (isTimeStamp) {
-				const ultimoStatus = new Date(
-					Math.floor(whatsApp?.statusImportMessages)
-				).getTime();
-				const dataLimite = +add(ultimoStatus, { seconds: +35 }).getTime();
-				if (dataLimite > new Date().getTime()) {
-					return (
-						<>
-							<Button
-								disabled
-								style={{ marginLeft: 12 }}
-								size="small"
-								endIcon={
-									<CircularProgress
-										size={12}
-										className={classes.buttonProgress}
-									/>
-								}
-								variant="outlined"
-								color="primary"
-							>
-								{i18n.t("connections.buttons.preparing")}
-							</Button>
-						</>
-					);
-				}
-			}
-		}
 	};
 
 	const renderActionButtons = whatsApp => {
@@ -359,21 +234,17 @@ const Connections = () => {
 				{(whatsApp.status === "CONNECTED" ||
 					whatsApp.status === "PAIRING" ||
 					whatsApp.status === "TIMEOUT") && (
-						<>
-							<Button
-								size="small"
-								variant="outlined"
-								color="secondary"
-								onClick={() => {
-									handleOpenConfirmationModal("disconnect", whatsApp.id);
-								}}
-							>
-								{i18n.t("connections.buttons.disconnect")}
-							</Button>
-
-							{renderImportButton(whatsApp)}
-						</>
-					)}
+					<Button
+						size="small"
+						variant="outlined"
+						color="secondary"
+						onClick={() => {
+							handleOpenConfirmationModal("disconnect", whatsApp.id);
+						}}
+					>
+						{i18n.t("connections.buttons.disconnect")}
+					</Button>
+				)}
 				{whatsApp.status === "OPENING" && (
 					<Button size="small" variant="outlined" disabled color="default">
 						{i18n.t("connections.buttons.connecting")}
@@ -458,53 +329,8 @@ const Connections = () => {
 							</Button>
 						)}
 					/>
-					
-					<Button
-						variant="contained"
-						color="primary"
-						onClick={() => openInNewTab(`https://wa.me/${process.env.REACT_APP_NUMBER_SUPPORT}`)}
-						
-					>
-						<WhatsApp style={{ marginRight: "5px"}} />
-						<span>{i18n.t("suporte")}</span>
-					</Button>
 				</MainHeaderButtonsWrapper>
 			</MainHeader>
-			{statusImport?.all ? (
-				<>
-					<div style={{ margin: "auto", marginBottom: 12 }}>
-						<Card className={classes.root}>
-							<CardContent className={classes.content}>
-								<Typography component="h5" variant="h5">
-									{statusImport?.this === -1 ? i18n.t("connections.buttons.preparing") : i18n.t("connections.buttons.importing")}
-								</Typography>
-								{statusImport?.this === -1 ?
-									<Typography component="h6" variant="h6" align="center">
-										<CircularProgress
-											size={24}
-										/>
-									</Typography>
-									:
-									<>
-										<Typography component="h6" variant="h6" align="center">
-											{`${i18n.t(`connections.typography.processed`)} ${statusImport?.this} ${i18n.t(`connections.typography.in`)} ${statusImport?.all}  ${i18n.t(`connections.typography.date`)}: ${statusImport?.date} `}
-										</Typography>
-										<Typography align="center">
-											<CircularProgressWithLabel
-												style={{ margin: "auto" }}
-												value={(statusImport?.this / statusImport?.all) * 100}
-											/>
-										</Typography>
-									</>
-
-
-								}
-
-							</CardContent>
-						</Card>
-					</div>
-				</>
-			) : null}
 			<Paper className={classes.mainPaper} variant="outlined">
 				<Table size="small">
 					<TableHead>
